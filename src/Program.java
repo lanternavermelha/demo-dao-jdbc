1600bdfe90ea5278e300613bc599ac37c86be083
lanternavermelha <f_figueira@hotmail.com>
2019-12-04 18:53:37 +0000

    Seller entity class

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,7 @@
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -6,7 +9,9 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Filipe", "filipe@gmail", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
