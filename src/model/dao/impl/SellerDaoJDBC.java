7f0df8ecc249d27d10a23ced54f270e6a31bf6c5
lanternavermelha <f_figueira@hotmail.com>
2019-12-04 19:09:20 +0000

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
